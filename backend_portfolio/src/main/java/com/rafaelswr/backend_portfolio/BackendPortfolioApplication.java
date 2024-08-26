eb4922a8a163f7f7b07328e19659d5d4ad63f03b
rafaelswr <rafaels.borges91@gmail.com>
2024-08-26 22:52:42 +0100

    initial on backend
    - set up project

---
+++ backend_portfolio/src/main/java/com/rafaelswr/backend_portfolio/BackendPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.backend_portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendPortfolioApplication.class, args);
+    }
+
+}
---
+++ backend_portfolio/src/test/java/com/rafaelswr/backend_portfolio/BackendPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelswr.backend_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
